ba0df3c3d3e19ee7a23a2b0d758e8183f9f49d06
Muaz Abdeen <muaz.abdeen@cs.huji.ac.il>
2023-06-21 19:52:05 +0300

    final

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -4,22 +4,18 @@ from translator import english_to_french, french_to_english
 
 class TestE2F(unittest.TestCase):
     def test_positive(self):
-        self.assertEqual(english_to_french('hello'), 'bonjour',
-                         msg='E2F POSITIVE PASSED')
+        self.assertEqual(english_to_french('hello'), 'bonjour')
 
     def test_negative(self):
-        self.assertNotEqual(english_to_french('hello'), 'merci',
-                            msg='E2F NEGATIVE PASSED')
+        self.assertNotEqual(english_to_french('hello'), 'merci')
 
 
 class TestF2E(unittest.TestCase):
     def test_positive(self):
-        self.assertEqual(french_to_english('Bonjour'), 'Hello',
-                         msg='F2E POSITIVE PASSED')
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
 
     def test_negative(self):
-        self.assertNotEqual(french_to_english('merci'), 'Hello',
-                            msg='F2E NEGATIVE PASSED')
+        self.assertNotEqual(french_to_english('merci'), 'Hello')
 
 
 if __name__ == '__main__':
